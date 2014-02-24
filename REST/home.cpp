71bf7260ec3c10a85ffd16e03d98d593dce739b3
M.S. Babaei <info@babaei.net>
2014-02-24 12:36:48 +0330

    Minimal functional layout

--- REST/home.cpp
+++ REST/home.cpp
@@ -1,3 +1,6 @@
+#include <Wt/WAnchor>
+#include <Wt/WImage>
+#include <Wt/WLink>
 #include <Wt/WText>
 #include <Wt/WWidget>
 #include "home.hpp"
@@ -43,14 +46,39 @@ WWidget *Home::Layout()
     Div *divHeader = new Div(root, "divHeader");
     (void)divHeader;
 
+    Div* divSocialMedia = new Div(divHeader, "divSocialMedia");
+    WImage *facebookImage = new WImage(WLink("facebook.png"), L"Follow us on Facebook");
+    WAnchor *facebookAnchor = new WAnchor(WLink("http://www.facebook.com/kargozaar"),
+                                          facebookImage, divSocialMedia);
+    (void)facebookAnchor;
+    WImage *twitterImage = new WImage(WLink("twitter.png"), L"Follow us on twitter");
+    WAnchor *twitterAnchor = new WAnchor(WLink("http://www.twitter.com/kargozaar"),
+                                         twitterImage, divSocialMedia);
+    (void)twitterAnchor;
+
+    Div* divLogo = new Div(divHeader, "divLogo");
+    WImage *logoImage = new WImage(WLink("logo.png"), L"کارگزار");
+    WAnchor *logoAnchor = new WAnchor(WLink("."), logoImage, divLogo);
+    (void)logoAnchor;
+
+    Div *divSeparator01 = new Div(root, "divSeparator01", "divHorizontalSeparator");
+    (void)divSeparator01;
+
     Div *divSplash = new Div(root, "divSplash");
-    Div *divSplashPic = new Div(divSplash, "divSplashPic");
+    Div *divSplashInner = new Div(divSplash, "divSplashInner");
+    Div *divSplashPic = new Div(divSplashInner, "divSplashPic");
     (void)divSplashPic;
-    Div *divSplashText = new Div(divSplash, "divSplashText");
+    Div *divSplashText = new Div(divSplashInner, "divSplashText");
     (void)divSplashText;
+
+    Div *divSeparator02 = new Div(root, "divSeparator02", "divHorizontalSeparator");
+    (void)divSeparator02;
 
     Div *divDownloads = new Div(root, "divDownloads");
     (void)divDownloads;
+
+    Div *divSeparator03 = new Div(root, "divSeparator03", "divHorizontalSeparator");
+    (void)divSeparator03;
 
     Div *divStockTable = new Div(root, "divStockTable");
     (void)divStockTable;
